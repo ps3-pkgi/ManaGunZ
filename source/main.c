76947c0749275d7121455ed1790ebab42541ed8d
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-12-10 22:46:21 +0800

    fix

--- source/main.c
+++ source/main.c
@@ -9,42 +9,25 @@
 int main()
 {
 #ifdef _BDVD_BUILD_
-	#ifdef FILEMANAGER
-		sysProcessExitSpawn2("/dev_bdvd/PS3_GAME/USRDIR/FileManager.self", NULL, NULL, NULL, 0, 1001, SYS_PROCESS_SPAWN_STACK_SIZE_1M);
-		return 0;
-	#endif
 	sysProcessExitSpawn2("/dev_bdvd/PS3_GAME/USRDIR/ManaGunZ.self", NULL, NULL, NULL, 0, 1001, SYS_PROCESS_SPAWN_STACK_SIZE_1M);
 	return 0;
 #endif
-
-#ifdef FILEMANAGER
-	sysProcessExitSpawn2("/dev_hdd0/game/FILEMANAG/USRDIR/FileManager.self", NULL, NULL, NULL, 0, 1001, SYS_PROCESS_SPAWN_STACK_SIZE_1M);
-	return 0;
-#endif
-
  
 	FILE* fp;
 	char self_path[50];
 	char launcher_id[10]={0};
 
 	fp = fopen("/dev_hdd0/vsh/pushlist/game.dat", "rb");
-	if(fp!=NULL) {	
+	if(fp!=NULL) {
 		fread(launcher_id, 9, 1, fp);
 		fclose(fp);
 	} else {
 // for RPCS3
-#ifdef FILEMANAGER
-		strcpy(launcher_id, "FILEMANAG");
-#else
 		strcpy(launcher_id, "MANAGUNZ0");
-#endif
 	}
 	launcher_id[9]=0;
-	
 	sprintf(self_path, "/dev_hdd0/game/%s/USRDIR/ManaGunZ.self", launcher_id);
-	
 	sysProcessExitSpawn2(self_path, NULL, NULL, NULL, 0, 1001, SYS_PROCESS_SPAWN_STACK_SIZE_1M);
-	
 	return 0;
 
 }
